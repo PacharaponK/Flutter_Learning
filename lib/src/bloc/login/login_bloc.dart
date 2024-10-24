2b044f1897bf5022b6b835b93a58e63123bcdc03
พชรพล เกตุแก้ว <ballxlenver7@gmail.com>
2024-10-24 17:31:55 +0700

    bloc #4

--- lib/src/bloc/login/login_bloc.dart
+++ lib/src/bloc/login/login_bloc.dart
@@ -1,6 +1,7 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:meta/meta.dart';
+import 'package:workshop01/src/models/user.dart';
 
 part 'login_event.dart';
 part 'login_state.dart';
@@ -9,15 +10,23 @@ class LoginBloc extends Bloc<LoginEvent, LoginState> {
   LoginBloc() : super(LoginState()) {
     //add
 
-    on<LoginEventAdd>((event, emit) async {
-      await Future.delayed(Duration(seconds: 1));
+    on<LoginEventAdd>((event, emit)  {
       emit(state.copyWith(count: state.count + 1));
     });
 
     //remove
-    on<LoginEventRemove>((event, emit) async {
-      await Future.delayed(Duration(seconds: 1));
+    on<LoginEventRemove>((event, emit)  {
       emit(state.copyWith(count: state.count - 1));
+    });
+
+    //login
+    on<LoginEventLogin>((event, emit) {
+      if (event.payload.username == "admin" && event.payload.password == "1234") {
+        emit(state.copyWith(isAuthened: true));
+      }
+      else {
+        emit(state.copyWith(isAuthened: false));
+      }
     });
   }
 }
